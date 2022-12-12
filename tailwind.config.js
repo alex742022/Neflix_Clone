7374796e5881e532c19fab1cee490d6e47cf8bc9
alex742022 <alexis.aquino.lumbera7@gmail.com>
2022-12-12 14:57:41 +0800

    added tailwind framework

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'landingPageNetflix': 'url("../public/BackgroundImage/LandingBackgroundImg.jpg")',
+      }, 
+    },
   },
   plugins: [],
 }
