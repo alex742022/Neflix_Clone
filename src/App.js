160fa1bc430697d3eb167e78167e8bc2b79c3976
alex742022 <alexis.aquino.lumbera7@gmail.com>
2022-12-12 15:11:06 +0800

    added component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react'
-
+import React from "react";
+import Home from "./Components/home"
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    // <div className="grid grid-rows-[1fr_1fr] sm:grid-cols-[minmax(0px,_400px)_1fr]">
+    //   <div className="border">
+    //     <h1 className="text-3xl font-bold underline ">Column1</h1>
+    //   </div>
+    //   <div><h1 className="text-3xl font-light border">Column2asdsadsadsadsadasdasd</h1></div>  
+    // </div>
+    <div>
+      <Home/>
+    </div>
   );
 }
 
