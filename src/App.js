07c78f7b60a5c21f3742c1791626f52db18eb8ce
alex742022 <alexis.aquino.lumbera7@gmail.com>
2022-12-21 08:52:07 +0800

    added new Components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
-import Home from "./Components/home"
+import Home from "./Components/LandingPage/home";
+import Section2 from "./Components/LandingPage/section2";
+import Footer from "./Components/LandingPage/footer";
+import Main from "./Components/Main/main";
+
 function App() {
   return (
     // <div className="grid grid-rows-[1fr_1fr] sm:grid-cols-[minmax(0px,_400px)_1fr]">
     //   <div className="border">
     //     <h1 className="text-3xl font-bold underline ">Column1</h1>
     //   </div>
-    //   <div><h1 className="text-3xl font-light border">Column2asdsadsadsadsadasdasd</h1></div>  
+    //   <div><h1 className="text-3xl font-light border">Column2asdsadsadsadsadasdasd</h1></div>
     // </div>
     <div>
-      <Home/>
+      <Home />
+      <Section2 />
+      <Main />
+      <Footer />
     </div>
   );
 }
