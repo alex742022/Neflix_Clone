6fa49da8d2958c97b444ed9a170cf29510fe94fa
alex742022 <alexis.aquino.lumbera7@gmail.com>
2022-12-22 11:40:50 +0800

    styled the Container of ALlMovies

--- src/Components/Main/main.jsx
+++ src/Components/Main/main.jsx
@@ -55,7 +55,7 @@ export default function Main() {
           <h1 className="text-white mt-[20px] text-[20px] sm:text-[25px] sm:mt-[50px]">
             Movies
           </h1>
-          <div className="grid grid-cols-2 gap-[20px] mt-[30px] sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
+          <div className="grid grid-cols-3 gap-[15px] mt-[30px] sm:gap-[20px] md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
             <AllMovies/>
           </div>
         </div>
