61e471784c8c9cc6eeabac47e50a0ff5ef2d4ede
alex742022 <alexis.aquino.lumbera7@gmail.com>
2022-12-22 21:55:59 +0800

    added an loading animation

--- src/Components/API/allMovies.jsx
+++ src/Components/API/allMovies.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import ReactLoading from "react-loading";
+import LoadingAnimation from "../LoadAnimation/loadingAnimation";
 export default function AllMovie() {
-  const [allMovies, setAllMovies] = useState([]);
+  const [allMovies, setAllMovies] = useState(null);
   //   loading animation
   const [loadingAllMovies, setLoadingAllMovies] = useState(true);
   const allMovies_API =
@@ -22,46 +23,49 @@ export default function AllMovie() {
       setLoadingAllMovies(false);
     }, 40000);
   }, [allMovies_API]);
-
+console.log(allMovies);
   return (
     <>
-      {allMovies.map((movies) => {
-        return (
-          <div
-            className="w-[100%] bg-[rgb(30,30,30)] rounded-md overflow-hidden flex-shrink-0"
-            key={movies.id}
-          >
-            <div className="aspect-[2/3]">
-              <div className="w-full h-fit">
-                {movies.poster_path ? (
-                  <img
-                    className="w-full"
-                    src={`https://www.themoviedb.org/t/p/w440_and_h660_face${movies.poster_path}`}
-                    alt="Featured"
-                  />
-                ) : (
-                  // Loading animation
-                  <div className="w-full h-full flex">
-                    <div className="w-[100%] flex items-center justify-center bg-[rgb(30,30,30)] px-[20px]">
-                      {loadingAllMovies ? (
-                        <ReactLoading
-                          type={"spin"}
-                          color={"#737373"}
-                          width={40}
-                        />
-                      ) : (
-                        // when API failed to render
-                        <h1 className="text-white text-center text-sm">
-                          Error check your connection
-                        </h1>
-                      )}
+      {!allMovies ? (
+          <LoadingAnimation/>
+      ) : (
+        allMovies.map((movies) => {
+          return (
+            <div
+              className="w-[100%] bg-[rgb(30,30,30)] rounded-md overflow-hidden flex-shrink-0"
+              key={movies.id}
+            >
+              <div className="aspect-[2/3]">
+                <div className="w-full h-fit">
+                  {movies.poster_path ? (
+                    <img
+                      className="w-full"
+                      src={`https://www.themoviedb.org/t/p/w440_and_h660_face${movies.poster_path}`}
+                      alt="Featured"
+                    />
+                  ) : (
+                    // Loading animation
+                    <div className="w-full h-full flex">
+                      <div className="w-[100%] flex items-center justify-center bg-[rgb(30,30,30)] px-[20px]">
+                        {loadingAllMovies ? (
+                          <ReactLoading
+                            type={"spin"}
+                            color={"#737373"}
+                            width={40}
+                          />
+                        ) : (
+                          // when API failed to render
+                          <h1 className="text-white text-center text-sm">
+                            Error check your connection
+                          </h1>
+                        )}
+                      </div>
                     </div>
-                  </div>
-                )}
-              </div>
-              <div className="relative w-full h-fit">
-                <div className="absolute left-[50%] translate-x-[-50%] top-[-10px] w-[150%] h-[120px] blur-xl bg-black"></div>
-                <div className="relative mt-[10px] px-[10px] text-white">
+                  )}
+                </div>
+                <div className="relative w-full h-fit">
+                  <div className="absolute left-[50%] translate-x-[-50%] top-[-10px] w-[150%] h-[120px] blur-xl bg-black"></div>
+                  <div className="relative mt-[10px] px-[10px] text-white">
                     <h1 className="allMoviesTitle text-[11px] sm:text-[18px] md:text-[20px]">
                       {movies.name}
                       {movies.title}
@@ -71,11 +75,12 @@ export default function AllMovie() {
                       {movies.release_date}
                     </small>
                   </div>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })
+      )}
     </>
   );
 }
