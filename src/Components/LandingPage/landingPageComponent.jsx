7e42a6c976275b3ed52c9489dca3a3fdebcc9bd6
alex742022 <alexis.aquino.lumbera7@gmail.com>
2022-12-26 14:30:43 +0800

    added a state for footer condition, change the value of ul, fix some bugs, and remove comments

--- src/Components/LandingPage/landingPageComponent.jsx
+++ src/Components/LandingPage/landingPageComponent.jsx
@@ -28,79 +28,78 @@ export default function LandingPageComponent() {
     footerTextEleven: "Media Center",
     footerTextTwelve: "Jobs",
     footerTextThirteen: "Buy Gift Cards",
-    footerTextOneFourTeen: "Terms of Use",
+    footerTextOneFourteen: "Terms of Use",
     footerTextOneFifthteen: "Cookie Preferences",
     footerTextOneSixteen: "Contact Us",
     footerTextOneSeventeen: "Legal Notices",
   });
-
+  // for Navigation
   const [chooseLanguage, setChooseLanguage] = useState(false);
+  // for Footer
+  const [chooseLanguageFooter, setChooseLanguageFooter] = useState(false);
 
   const changeLangtoEng = () => {
-    setChangeLanguage(() => ({
-      language: "English",
-      login: "Sign In",
-      header: "Unlimited movies, TV shows, and more.",
-      textOne: "Watch anywhere. Cancel anytime.",
-      textTwo:
-        "Ready to watch? Enter your email to create or restart your membership.",
-      textButton: "Get Started",
-      textOneSection: "Enjoy on your TV.",
-      textTwoSection:
-        "Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.",
-      // footerTextOne: "FAQ",
-      // footerTextTwo: "Account",
-      footerTextThree: "Investor Relations",
-      footerTextFour: "Redeem Gifts Cards",
-      footerTextFive: "Ways to Watch",
-      // footerTextSix: "Privacy",
-      // footerTextSeven: "Corporate Information",
-      // footerTextEight: "Speed Test",
-      footerTextNine: "Only on Netflix",
-      // footerTextTeen: "Help Center",
-      // footerTextEleven: "Media Center",
-      footerTextTwelve: "Jobs",
-      footerTextThirteen: "Buy Gift Cards",
-      footerTextOneFourTeen: "Terms of Use",
-      footerTextOneFifthteen: "Cookie Preferences",
-      footerTextOneSixteen: "Contact Us",
-      footerTextOneSeventeen: "Legal Notices",
-    }));
+    setChangeLanguage((prevChooseLanguage) => {
+      return {
+        ...prevChooseLanguage,
+        language: "English",
+        login: "Sign In",
+        header: "Unlimited movies, TV shows, and more.",
+        textOne: "Watch anywhere. Cancel anytime.",
+        textTwo:
+          "Ready to watch? Enter your email to create or restart your membership.",
+        textButton: "Get Started",
+        textOneSection: "Enjoy on your TV.",
+        textTwoSection:
+          "Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.",
+        footerTextThree: "Investor Relations",
+        footerTextFour: "Redeem Gifts Cards",
+        footerTextFive: "Ways to Watch",
+        footerTextNine: "Only on Netflix",
+        footerTextTwelve: "Jobs",
+        footerTextThirteen: "Buy Gift Cards",
+        footerTextOneFourteen: "Terms of Use",
+        footerTextOneFifthteen: "Cookie Preferences",
+        footerTextOneSixteen: "Contact Us",
+        footerTextOneSeventeen: "Legal Notices",
+      };
+    });
   };
   const changeLangtoTag = () => {
-    setChangeLanguage(() => ({
-      language: "Filipino",
-      login: "Mag-sign In",
-      header: "Unlimited na pelikula, TV show, at marami pang iba.",
-      textOne: "Manood kahit saan. Mag-cancel kahit kailan.",
-      textTwo:
-        "Handa nang manood? Ilagay ang email mo para gawin o simulan ulit ang membership mo.",
-      textButton: "Magsimula",
-      textOneSection: "Mag-enjoy sa TV mo.",
-      textTwoSection:
-        "Manood sa mga Smart TV, Playstation, Xbox, Chromecast, Apple TV, Blu-ray player, at marami pang iba.",
-      // footerTextOne: "FAQ",
-      // footerTextTwo: "Account",
-      footerTextThree: "Mga Pakikipag-ugnayan sa Investor",
-      footerTextFour: "Mag-redeem ng mga Gift Card",
-      footerTextFive: "Mga Paraan para Manood",
-      // footerTextSix: "Privacy",
-      // footerTextSeven: "Corporate Information",
-      // footerTextEight: "Speed Test",
-      footerTextNine: "Sa Netflix lang",
-      footerTextTeen: "Help Center",
-      footerTextEleven: "Media Center",
-      footerTextTwelve: "Mga Trabaho",
-      footerTextThirteen: "Bumili ng mga Gift Card",
-      footerTextOneFourTeen: "Mga Tuntunin ng Paggamit",
-      footerTextOneFifthteen: "Mga Preference sa Cookie",
-      footerTextOneSixteen: "Makipag-ugnayan sa Amin",
-      footerTextOneSeventeen: "Mga Legal na Abiso",
-    }));
+    setChangeLanguage((prevChooseLanguage) => {
+      return {
+        ...prevChooseLanguage,
+        language: "Filipino",
+        login: "Mag-sign In",
+        header: "Unlimited na pelikula, TV show, at marami pang iba.",
+        textOne: "Manood kahit saan. Mag-cancel kahit kailan.",
+        textTwo:
+          "Handa nang manood? Ilagay ang email mo para gawin o simulan ulit ang membership mo.",
+        textButton: "Magsimula",
+        textOneSection: "Mag-enjoy sa TV mo.",
+        textTwoSection:
+          "Manood sa mga Smart TV, Playstation, Xbox, Chromecast, Apple TV, Blu-ray player, at marami pang iba.",
+        footerTextThree: "Mga Pakikipag-ugnayan sa Investor",
+        footerTextFour: "Mag-redeem ng mga Gift Card",
+        footerTextFive: "Mga Paraan para Manood",
+        footerTextNine: "Sa Netflix lang",
+        footerTextTeen: "Help Center",
+        footerTextEleven: "Media Center",
+        footerTextTwelve: "Mga Trabaho",
+        footerTextThirteen: "Bumili ng mga Gift Card",
+        footerTextOneFourteen: "Mga Tuntunin ng Paggamit",
+        footerTextOneFifthteen: "Mga Preference sa Cookie",
+        footerTextOneSixteen: "Makipag-ugnayan sa Amin",
+        footerTextOneSeventeen: "Mga Legal na Abiso",
+      };
+    });
   };
 
   const choseLanguage = () => {
     setChooseLanguage((prevChooseLanguage) => !prevChooseLanguage);
+  };
+  const choseLanguageFooter = () => {
+    setChooseLanguageFooter((prevChooseLanguage) => !prevChooseLanguage);
   };
 
   return (
@@ -128,7 +127,13 @@ export default function LandingPageComponent() {
         </div>
       </section>
       <Section2 changeLang={changeLanguage} />
-      <Footer changeLang={changeLanguage} />
+      <Footer
+        changeLang={changeLanguage}
+        chooseLang={chooseLanguageFooter}
+        changetoEng={changeLangtoEng}
+        changetoTag={changeLangtoTag}
+        choseLang={choseLanguageFooter}
+      />
     </>
   );
 }
