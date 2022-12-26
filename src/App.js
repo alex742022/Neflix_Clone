92ed3dfdb1a58835894b284895d5ee566cf2ddd9
alex742022 <alexis.aquino.lumbera7@gmail.com>
2022-12-26 21:11:28 +0800

    removed comment and import Router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React from "react";
 import LandingPageComponent from "./Components/LandingPage/landingPageComponent";
 import Main from "./Components/Main/main";
-
+//Router 
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
-    // <div className="grid grid-rows-[1fr_1fr] sm:grid-cols-[minmax(0px,_400px)_1fr]">
-    //   <div className="border">
-    //     <h1 className="text-3xl font-bold underline ">Column1</h1>
-    //   </div>
-    //   <div><h1 className="text-3xl font-light border">Column2asdsadsadsadsadasdasd</h1></div>
-    // </div>
-    <div>
-      <LandingPageComponent />
-      <Main />
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<LandingPageComponent />} />
+        <Route path="/started" element={<Main />} />
+      </Routes>
+    </>
   );
 }
 
