db5518bd5d3b7a0d6d729d7ff9465521c9b406be
alex742022 <alexis.aquino.lumbera7@gmail.com>
2022-12-28 10:55:14 +0800

    imported the Function that nested the useContext

--- src/Components/LandingPage/footer.jsx
+++ src/Components/LandingPage/footer.jsx
@@ -2,111 +2,150 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGlobe } from "@fortawesome/free-solid-svg-icons";
 import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+// import Context
+import {
+  CurrentLanguage,
+  LanguageChooseFooter,
+  LanguagesShowFooter,
+  LanguageToEnglish,
+  LanguageToTagalog,
+} from "./changeLanguageFunction";
 
-export default function Footer(prop) {
+export default function Footer() {
+  const languages = CurrentLanguage();
+  // distructuring
+  const {
+    language,
+    footerTextOne,
+    footerTextTwo,
+    footerTextThree,
+    footerTextFour,
+    footerTextFive,
+    footerTextSix,
+    footerTextSeven,
+    footerTextEight,
+    footerTextNine,
+    footerTextTeen,
+    footerTextEleven,
+    footerTextTwelve,
+    footerTextThirteen,
+    footerTextFourteen,
+    footerTextFifthteen,
+    footerTextSixteen,
+    footerTextSeventeen,
+    footerTextEighteen
+  } = languages;
+
+  const languageToTagalog = LanguageToTagalog();
+  const languageToEnglish = LanguageToEnglish();
+  const languageChooseFooter = LanguageChooseFooter();
+  const languageShowFooter = LanguagesShowFooter();
+
   return (
     <footer className="bg-black border-t-8 border-[#222222] text-[#737373]">
       <div className="px-[20px] max-w-screen-lg mx-auto">
         <div className="px-[20px] pt-[50px] pb-[30px] sm:px-[40px]">
-          <h1 className="text-[17px]">{prop.changeLang.footerTextOneEighteen} {prop.changeLang.footerTextOneSixteen}.</h1>
+          <h1 className="text-[17px]">
+            {footerTextEighteen} {footerTextSixteen}.
+          </h1>
           <div className="flex justify-between gap-[30px] mt-[30px] sm:hidden">
             <li className="flex flex-col gap-[10px] w-[50%] text-sm list-none">
-              <ul>{prop.changeLang.footerTextOne}</ul>
-              <ul>{prop.changeLang.footerTextTwo}</ul>
-              <ul>{prop.changeLang.footerTextThree}</ul>
-              <ul>{prop.changeLang.footerTextFour}</ul>
-              <ul>{prop.changeLang.footerTextFive}</ul>
-              <ul>{prop.changeLang.footerTextSix}</ul>
-              <ul>{prop.changeLang.footerTextSeven}</ul>
-              <ul>{prop.changeLang.footerTextEight}</ul>
-              <ul>{prop.changeLang.footerTextNine}</ul>
+              <ul>{footerTextOne}</ul>
+              <ul>{footerTextTwo}</ul>
+              <ul>{footerTextThree}</ul>
+              <ul>{footerTextFour}</ul>
+              <ul>{footerTextFive}</ul>
+              <ul>{footerTextSix}</ul>
+              <ul>{footerTextSeven}</ul>
+              <ul>{footerTextEight}</ul>
+              <ul>{footerTextNine}</ul>
             </li>
             <li className="flex flex-col gap-[10px] w-[50%] text-sm list-none">
-              <ul>{prop.changeLang.footerTextTeen}</ul>
-              <ul>{prop.changeLang.footerTextEleven}</ul>
-              <ul>{prop.changeLang.footerTextTwelve}</ul>
-              <ul>{prop.changeLang.footerTextThirteen}</ul>
-              <ul>{prop.changeLang.footerTextOneFourteen}</ul>
-              <ul>{prop.changeLang.footerTextOneFifthteen}</ul>
-              <ul>{prop.changeLang.footerTextOneSixteen}</ul>
-              <ul>{prop.changeLang.footerTextOneSeventeen}</ul>
+              <ul>{footerTextTeen}</ul>
+              <ul>{footerTextEleven}</ul>
+              <ul>{footerTextTwelve}</ul>
+              <ul>{footerTextThirteen}</ul>
+              <ul>{footerTextFourteen}</ul>
+              <ul>{footerTextFifthteen}</ul>
+              <ul>{footerTextSixteen}</ul>
+              <ul>{footerTextSeventeen}</ul>
             </li>
           </div>
 
           <div className="hidden sm:flex sm:justify-between sm:gap-[30px] mt-[30px] md:hidden">
             <li className="flex flex-col gap-[10px] w-[50%] text-sm list-none">
-              <ul>{prop.changeLang.footerTextOne}</ul>
-              <ul>{prop.changeLang.footerTextEleven}</ul>
-              <ul>{prop.changeLang.footerTextThree}</ul>
-              <ul>{prop.changeLang.footerTextFour}</ul>
-              <ul>{prop.changeLang.footerTextOneFourteen}</ul>
-              <ul>{prop.changeLang.footerTextSeven}</ul>
-              <ul>{prop.changeLang.footerTextOneSeventeen}</ul>
+              <ul>{footerTextOne}</ul>
+              <ul>{footerTextEleven}</ul>
+              <ul>{footerTextThree}</ul>
+              <ul>{footerTextFour}</ul>
+              <ul>{footerTextFourteen}</ul>
+              <ul>{footerTextSeven}</ul>
+              <ul>{footerTextSeventeen}</ul>
             </li>
             <li className="flex flex-col gap-[10px] w-[50%] text-sm list-none">
-              <ul>{prop.changeLang.footerTextTeen}</ul>
-              <ul>{prop.changeLang.footerTextThree}</ul>
-              <ul>{prop.changeLang.footerTextThirteen}</ul>
-              <ul>{prop.changeLang.footerTextSix}</ul>
-              <ul>{prop.changeLang.footerTextOneSixteen}</ul>
-              <ul>{prop.changeLang.footerTextNine}</ul>
+              <ul>{footerTextTeen}</ul>
+              <ul>{footerTextThree}</ul>
+              <ul>{footerTextThirteen}</ul>
+              <ul>{footerTextSix}</ul>
+              <ul>{footerTextSixteen}</ul>
+              <ul>{footerTextNine}</ul>
             </li>
             <li className="flex flex-col gap-[10px] w-[50%] text-sm list-none">
-              <ul>{prop.changeLang.footerTextTwo}</ul>
-              <ul>{prop.changeLang.footerTextTwelve}</ul>
-              <ul>{prop.changeLang.footerTextThirteen}</ul>
-              <ul>{prop.changeLang.footerTextFive}</ul>
-              <ul>{prop.changeLang.footerTextOneFifthteen}</ul>
-              <ul>{prop.changeLang.footerTextEight}</ul>
+              <ul>{footerTextTwo}</ul>
+              <ul>{footerTextTwelve}</ul>
+              <ul>{footerTextThirteen}</ul>
+              <ul>{footerTextFive}</ul>
+              <ul>{footerTextFifthteen}</ul>
+              <ul>{footerTextEight}</ul>
             </li>
           </div>
 
           <div className="hidden md:flex md:justify-between md:gap-[30px] mt-[30px]">
             <li className="flex flex-col gap-[10px] w-[50%] text-sm list-none">
-              <ul>{prop.changeLang.footerTextOne}</ul>
-              <ul>{prop.changeLang.footerTextThree}</ul>
-              <ul>{prop.changeLang.footerTextFive}</ul>
-              <ul>{prop.changeLang.footerTextSeven}</ul>
-              <ul>{prop.changeLang.footerTextNine}</ul>
+              <ul>{footerTextOne}</ul>
+              <ul>{footerTextThree}</ul>
+              <ul>{footerTextFive}</ul>
+              <ul>{footerTextSeven}</ul>
+              <ul>{footerTextNine}</ul>
             </li>
             <li className="flex flex-col gap-[10px] w-[50%] text-sm list-none">
-              <ul>{prop.changeLang.footerTextTeen}</ul>
-              <ul>{prop.changeLang.footerTextTwelve}</ul>
-              <ul>{prop.changeLang.footerTextOneFourteen}</ul>
-              <ul>{prop.changeLang.footerTextOneSixteen}</ul>
+              <ul>{footerTextTeen}</ul>
+              <ul>{footerTextTwelve}</ul>
+              <ul>{footerTextFourteen}</ul>
+              <ul>{footerTextSixteen}</ul>
             </li>
             <li className="flex flex-col gap-[10px] w-[50%] text-sm list-none">
-              <ul>{prop.changeLang.footerTextTwo}</ul>
-              <ul>{prop.changeLang.footerTextFour}</ul>
-              <ul>{prop.changeLang.footerTextSix}</ul>
-              <ul>{prop.changeLang.footerTextEight}</ul>
+              <ul>{footerTextTwo}</ul>
+              <ul>{footerTextFour}</ul>
+              <ul>{footerTextSix}</ul>
+              <ul>{footerTextEight}</ul>
             </li>
             <li className="flex flex-col gap-[10px] w-[50%] text-sm list-none">
-              <ul>{prop.changeLang.footerTextEleven}</ul>
-              <ul>{prop.changeLang.footerTextThirteen}</ul>
-              <ul>{prop.changeLang.footerTextOneFifthteen}</ul>
-              <ul>{prop.changeLang.footerTextOneSeventeen}</ul>
+              <ul>{footerTextEleven}</ul>
+              <ul>{footerTextThirteen}</ul>
+              <ul>{footerTextFifthteen}</ul>
+              <ul>{footerTextSeventeen}</ul>
             </li>
           </div>
 
           <div
             className="relative text-[rgb(180,180,180)] mt-[30px] w-[145px]"
-            onClick={prop.choseLang}
+            onClick={languageShowFooter}
           >
             <button className="text-[17px] font-[400] px-[15px] py-[10px] border border-[rgba(250,250,250,.3)] w-full">
               <FontAwesomeIcon className="mr-[15px]" icon={faGlobe} />
-              {prop.changeLang.language}
+              {language}
               <FontAwesomeIcon className="ml-[10px]" icon={faCaretDown} />
             </button>
             <div
               className={`absolute flex flex-col w-full bg-[gray] cursor-pointer text-center ${
-                prop.chooseLang ? "visible" : "invisible"
+                languageChooseFooter ? "visible" : "invisible"
               }`}
             >
-              <span className="hover:bg-sky-700" onClick={prop.changetoEng}>
+              <span className="hover:bg-sky-700" onClick={languageToEnglish}>
                 English
               </span>
-              <span className="hover:bg-sky-700" onClick={prop.changetoTag}>
+              <span className="hover:bg-sky-700" onClick={languageToTagalog}>
                 Filipino
               </span>
             </div>
