56c8e340f2300e190f0386fb0556cfa6b9f8a78c
alex742022 <alexis.aquino.lumbera7@gmail.com>
2022-12-28 11:05:22 +0800

    removed the prop and repalced it by Context

--- src/Components/LandingPage/navigation.jsx
+++ src/Components/LandingPage/navigation.jsx
@@ -3,29 +3,37 @@ import Logo from "../../Assets/Netflix-Logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGlobe } from "@fortawesome/free-solid-svg-icons";
 import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
-
-export default function Navigation(prop) {
+// import Context
+import { LanguageToTagalog, LanguageToEnglish, LanguageChoose, LanguagesShow, CurrentLanguage} from "./changeLanguageFunction"
+export default function Navigation() {
+  const languages = CurrentLanguage();
+  // distructuring
+  const {language, login } = languages;  
+  const languageToTagalog = LanguageToTagalog();
+  const languageToEnglish = LanguageToEnglish();
+  const languageChoose = LanguageChoose();
+  const languageShow = LanguagesShow();
   return (
     <nav className="flex text-white justify-between items-center">
       <div className="w-[100px] sm:w-[120px] xl:w-[170px]">
         <img src={Logo} alt="Logo" />
       </div>
       <div className="flex items-center gap-[10px]">
-        <div className="relative z-10" onClick={prop.choseLang}>
+        <div className="relative z-10" onClick={languageShow}>
           <button className="border px-[10px] sm:py-[5px]">
             <FontAwesomeIcon icon={faGlobe} className="mr-[5px]" />
-            {prop.changeLang.language}
+            {language}
             <FontAwesomeIcon icon={faCaretDown} className="ml-[5px]" />
           </button>
           <div
             className={`absolute flex flex-col w-full bg-[gray] cursor-pointer text-center ${
-              prop.chooseLang ? "visible" : "invisible"
+               languageChoose? "visible" : "invisible"
             }`}
           >
-            <span className="hover:bg-sky-700" onClick={prop.changetoEng}>
+            <span className="hover:bg-sky-700" onClick={languageToEnglish}>
               English
             </span>
-            <span className="hover:bg-sky-700" onClick={prop.changetoTag}>
+            <span className="hover:bg-sky-700" onClick={languageToTagalog}>
               Filipino
             </span>
           </div>
@@ -35,7 +43,7 @@ export default function Navigation(prop) {
                                 sm:px-[20px] 
                                 sm:py-[5px]"
         >
-          {prop.changeLang.login}
+          {login}
         </button>
       </div>
     </nav>
