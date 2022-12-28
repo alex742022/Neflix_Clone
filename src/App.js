2043490dc4718cb837ececfb366ae4db9b51ec3f
alex742022 <alexis.aquino.lumbera7@gmail.com>
2022-12-28 10:53:24 +0800

    import useContext Hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
 import LandingPageComponent from "./Components/LandingPage/landingPageComponent";
 import Main from "./Components/Main/main";
-//Router 
+//Router
 import { Route, Routes } from "react-router-dom";
+//import the Context Provider from ./Components/LandingPage/changeLanguageFunction
+import { ChangeLanguageFunction } from "./Components/LandingPage/changeLanguageFunction";
 function App() {
   return (
+    // import Context Provider
     <>
-      <Routes>
-        <Route path="/Neflix_Clone" element={<LandingPageComponent />} />
-        <Route path="/started" element={<Main />} />
-      </Routes>
+      <ChangeLanguageFunction>
+        <Routes>
+          <Route path="/Neflix_Clone" element={<LandingPageComponent />} />
+          <Route path="/started" element={<Main />} />
+        </Routes>
+      </ChangeLanguageFunction>
     </>
   );
 }
